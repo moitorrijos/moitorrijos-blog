1188b25c1a2b2972a63daef4c4b1f49887b14a6d
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-01-28 12:21:32 -0500

    Move Helmet to main layout.

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,7 @@
 import React from 'react'
-import { Helmet } from 'react-helmet'
-import { graphql, useStaticQuery } from 'gatsby'
 
-const Header = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title,
-          siteurl,
-          description
-        }
-      }
-    }
-  `)
-  return (
-    <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>{data.site.siteMetadata.title}</title>
-        <link rel="canonical" href={data.site.siteMetadata.siteurl} />
-        <meta name="description" content={data.site.siteMetadata.description} />
-      </Helmet>
-      <div className="header-image"></div>
-    </>
-  )
-}
+const Header = () => (
+  <div className="header-image"></div>
+)
 
 export default Header
--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -3,13 +3,38 @@ import Navigation from './navigation'
 import Footer from './footer'
 import 'typeface-raleway'
 import '../styles/main.sass'
+import { Helmet } from 'react-helmet'
+import { graphql, useStaticQuery } from 'gatsby'
+import favicon from '../../static/favicon.ico'
 
-const MainLayout = ({ children }) => (
-  <div className="main-container">
-    <Navigation />
-      {children}
-    <Footer />
-  </div>
-)
+const MainLayout = ({ children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title,
+          siteurl,
+          description
+        }
+      }
+    }
+  `)
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{data.site.siteMetadata.title}</title>
+        <link rel="canonical" href={data.site.siteMetadata.siteurl} />
+        <link rel="icon" href={favicon} />
+        <meta name="description" content={data.site.siteMetadata.description} />
+      </Helmet>
+      <div className="main-container">
+        <Navigation />
+          {children}
+        <Footer />
+      </div>
+    </>
+  )
+}
 
 export default MainLayout
