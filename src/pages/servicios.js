1e69216d421363bac0466159d5b293b819c1f2bb
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-01-29 18:31:19 -0500

    Create servies page. Fix h1 font-size value.

---
+++ src/pages/servicios.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainLayout from '../components/main-layout'
+import Header from '../components/header'
+
+const Servicios = () => (
+  <MainLayout>
+    <Header />
+    <div className="main-content">
+      <div className="full-width-content">
+        <h1 className="content-title">Mis Servicios</h1>
+        <h2>Creación de Sitios Web a la Medida</h2>
+      </div>
+    </div>
+  </MainLayout>
+)
+
+export default Servicios
