675b87d84ad136afa5de95188c61dc1fce5da3e2
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-02-27 23:35:16 -0500

    Add whatsapp color. Add contact me sass file. Add personal projects mdx file and delete js file. Add contact me paragraph before form. Add contact me page with show honeypot function.

--- src/pages/contactame.js
+++ src/pages/contactame.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import MainLayout from '../components/main-layout'
 import Header from '../components/header'
+import WhatsappIcon from '../components/icons/whatsapp-icon'
+import '../styles/_contactame.sass'
 
-const ContactPage = () => (
+const ContactPage = () => {
+  function showHoneypot() {
+    console.log('Honeypot goes here...')
+  }
+  return (
   <MainLayout>
     <Header />
     <div className="main-content">
       <div className="full-width-content">
         <h1 className="centered">Contáctame</h1>
+        <div className="centered-text">
+          <p>
+            ¿Necesitas una tarea urgente?
+            <br/>
+            <button className="whatsapp" onClick={showHoneypot}>
+              Haz click aquí para conversar conmigo por WhatsApp{' '}
+              <WhatsappIcon />
+            </button>
+            <br/>
+            O llena el formulario abajo y te contactaré muy pronto.
+          </p>
+        </div>
       </div>
     </div>
   </MainLayout>
-)
+  )
+}
 
 export default ContactPage
