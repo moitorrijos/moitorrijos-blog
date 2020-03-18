b255cfc75ddf94ed38e928f2ec38ffd775d233e8
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-03-18 17:48:37 -0400

    Create SEO component and add to main-layout.

--- src/components/main-layout.js
+++ src/components/main-layout.js
@@ -3,39 +3,17 @@ import Navigation from "./navigation"
 import Footer from "./footer"
 import "typeface-raleway"
 import "../styles/main.sass"
-import { Helmet } from "react-helmet"
-import { graphql, useStaticQuery } from "gatsby"
-import favicon from "../../static/favicon.ico"
+import SEO from './seo.js'
 
-const MainLayout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-          siteurl
-          description
-        }
-      }
-    }
-  `)
-  return (
-    <>
-      <Helmet>
-        <html lang="es-PA" />
-        <meta charSet="utf-8" />
-        <title>{data.site.siteMetadata.title}</title>
-        <link rel="canonical" href={data.site.siteMetadata.siteurl} />
-        <link rel="icon" href={favicon} />
-        <meta name="description" content={data.site.siteMetadata.description} />
-      </Helmet>
-      <div className="main-container">
-        <Navigation />
-        {children}
-        <Footer />
-      </div>
-    </>
-  )
-}
+const MainLayout = ({ children }) => (
+  <>
+    <SEO />
+    <div className="main-container">
+      <Navigation />
+      {children}
+      <Footer />
+    </div>
+  </>
+)
 
 export default MainLayout
