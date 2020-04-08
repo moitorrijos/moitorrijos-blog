25d9b8f9da07d12460fc620ba7932ba892e88598
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2020-04-08 10:31:24 -0500

    Add content for sharing in LinkedIn and Pinterest. Move og:image to assets in src file.

--- src/components/seo.js
+++ src/components/seo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from 'gatsby'
-import ogImage from '../../static/juan-moises-torrijos-website.jpg'
+import ogImage from '../assets/juan-moises-torrijos-website.jpg'
 
 const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(graphql`
@@ -59,8 +59,16 @@ const SEO = ({ description, lang, meta, title }) => {
           content: metaDescription,
         },
         {
+          name: 'twitter:image',
+          content: ogImage
+        },
+        {
           name: `og:image`,
           content: ogImage
+        },
+        {
+          itemprop: 'image',
+          content:  ogImage
         }
       ].concat(meta)}
     >
